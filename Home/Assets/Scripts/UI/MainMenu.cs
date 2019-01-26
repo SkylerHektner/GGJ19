879060b83149cdbf86f220b13ecab583e89d6cb2
Artorias2718 <arturoordonez271@gmail.com>
2019-01-26 16:47:10 -0700

    Added a Start and Quit button to the Title Screen.

---
+++ Home/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	#region Variables (public)
+
+	#endregion
+
+	#region Variables (protected)
+
+	#endregion
+
+	#region Variables (private)
+
+	#endregion
+
+	#region Getters_Setters
+
+	#endregion
+
+	#region Unity
+	// Use this for initialization
+	private void Start()
+	{
+	
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+	
+	}
+	#endregion
+
+	#region Custom
+
+    public void LoadScene(int i_sceneIndex)
+    {
+        SceneManager.LoadScene(i_sceneIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+	#endregion
+}
