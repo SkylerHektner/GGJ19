0ba4964faa05dacaf9395718d9a27fd763281de3
Joey Weidman <u0790967@ad.utah.edu>
2019-01-25 22:00:30 -0700

    Collectable and Player Inventory

---
+++ Home/Assets/Scripts/Collectable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    [SerializeField] private string key;
+
+    private void OnCollisionEnter(Collision c)
+    {
+        if(c.gameObject.tag == "Player")
+        {
+            Pickup(c.gameObject, key);
+        }
+    }
+
+    private void Pickup(GameObject player, string key)
+    {
+        // Add the collectable to the player's inventory.
+        PlayerInventory inventory = player.GetComponent<PlayerInventory>();
+        inventory.AddToInventory(key);
+
+        Destroy(gameObject);
+    }
+}
