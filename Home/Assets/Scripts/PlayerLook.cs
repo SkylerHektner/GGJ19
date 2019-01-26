423a6cc14e35da59408ad99a07add677ec53bb95
Skyler Hektner <skylerhektner@gmail.com>
2019-01-25 21:03:44 -0700

    Added in player look support

---
+++ Home/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{ 
+    public float Sensitivity = 15F;
+
+    private float minimumX = -360F;
+    private float maximumX = 360F;
+
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+
+    private float rotationX = 0F;
+    private float rotationY = 0F;
+
+    private List<float> rotArrayX = new List<float>();
+    float rotAverageX = 0F;
+
+    private List<float> rotArrayY = new List<float>();
+    float rotAverageY = 0F;
+
+    public float frameCounter = 20;
+
+    Quaternion originalRotation;
+
+    void Update()
+    {
+        rotAverageY = 0f;
+        rotAverageX = 0f;
+
+        rotationY += Input.GetAxis("Mouse Y") * Sensitivity;
+        rotationX += Input.GetAxis("Mouse X") * Sensitivity;
+
+        rotArrayY.Add(rotationY);
+        rotArrayX.Add(rotationX);
+
+        if (rotArrayY.Count >= frameCounter)
+        {
+            rotArrayY.RemoveAt(0);
+        }
+        if (rotArrayX.Count >= frameCounter)
+        {
+            rotArrayX.RemoveAt(0);
+        }
+
+        for (int j = 0; j < rotArrayY.Count; j++)
+        {
+            rotAverageY += rotArrayY[j];
+        }
+        for (int i = 0; i < rotArrayX.Count; i++)
+        {
+            rotAverageX += rotArrayX[i];
+        }
+
+        rotAverageY /= rotArrayY.Count;
+        rotAverageX /= rotArrayX.Count;
+
+        rotAverageY = ClampAngle(rotAverageY, minimumY, maximumY);
+        rotAverageX = ClampAngle(rotAverageX, minimumX, maximumX);
+
+        Quaternion yQuaternion = Quaternion.AngleAxis(rotAverageY, Vector3.left);
+        Quaternion xQuaternion = Quaternion.AngleAxis(rotAverageX, Vector3.up);
+
+        transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+    }
+
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb)
+            rb.freezeRotation = true;
+        originalRotation = transform.localRotation;
+    }
+
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        angle = angle % 360;
+        if ((angle >= -360F) && (angle <= 360F))
+        {
+            if (angle < -360F)
+            {
+                angle += 360F;
+            }
+            if (angle > 360F)
+            {
+                angle -= 360F;
+            }
+        }
+        return Mathf.Clamp(angle, min, max);
+    }
+}
